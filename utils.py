af5e1817d6246a45ab3bb68f52fb3cb501bccd69
ArielleGantelet <arielle.gantelet@lagoon.nc>
2019-05-15 16:19:25 +0200

    + jit

--- utils.py
+++ utils.py
@@ -17,12 +17,12 @@ def calcul_nb_voisins(Z):
 
 def iteration_jeu(Z):
     """
-    Fonction qui prend en argument l'Ã©tat initial du jeu de la vie (sous forme de liste de liste).
-    Fonction qui retourne l'Ã©tat du jeu aprÃ¨s 1 itÃ©ration.
+    Fonction qui prend en argument l'état initial du jeu de la vie (sous forme de liste de liste).
+    Fonction qui retourne l'état du jeu après 1 itération.
     
-    La fonction fait appel Ã  la fonction qui calcule le nombre de voisins puis modifie
-    les cellules, qui sont reprÃ©sentÃ©es par des 0 ou des 1 dans la matrice Z,
-    en appliquant la rÃ¨gle de dÃ©cision expliquÃ©e ci dessus.
+    La fonction fait appel à la fonction qui calcule le nombre de voisins puis modifie
+    les cellules, qui sont représentées par des 0 ou des 1 dans la matrice Z,
+    en appliquant la règle de décision expliquée ci dessus.
     
     """
     
@@ -37,3 +37,38 @@ def iteration_jeu(Z):
     return Z
 
 
+from numba import jit
+
+
+@jit(nopython=True)
+def calcul_nb_voisins_fast(Z):
+    forme = len(Z), len(Z[0])
+    N = [[0, ] * (forme[0]) for i in range(forme[1])]
+    for x in range(1, forme[0] - 1):
+        for y in range(1, forme[1] - 1):
+            N[x][y] = Z[x-1][y-1]+Z[x][y-1]+Z[x+1][y-1] \
+            + Z[x-1][y] + 0 +Z[x+1][y] \
+            + Z[x-1][y+1]+Z[x][y+1]+Z[x+1][y+1]
+    return N
+
+
+@jit(nopython=True)
+def iteration_jeu_fast(Z):
+    """
+    Fonction qui prend en argument l'état initial du jeu de la vie (sous forme de liste de liste).
+    Fonction qui retourne l'état du jeu après 1 itération.
+    
+    La fonction fait appel à la fonction qui calcule le nombre de voisins puis modifie
+    les cellules, qui sont représentées par des 0 ou des 1 dans la matrice Z,
+    en appliquant la règle de dÃ©cision expliquée ci dessus.
+    
+    """
+    forme = len(Z), len(Z[0])
+    N = calcul_nb_voisins_fast(Z)
+    for x in range(1, forme[0] - 1):
+        for y in range(1,forme[1]-1):
+            if (Z[x][y] == 1) and (N[x][y] < 2 or N[x][y] > 3):
+                Z[x][y] = 0
+            elif (Z[x][y] == 0) and (N[x][y] == 3):
+                Z[x][y] = 1
+    return Z
